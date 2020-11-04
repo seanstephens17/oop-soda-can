a16aedd845ea4947dcefe17fffef3986c013eafd
seanstephens17 <D00211442@student.dkit.ie>
2020-11-04 11:32:24 +0000

    Surface Area and Volume first attempt

--- src/main/java/oop/SodaCan.java
+++ src/main/java/oop/SodaCan.java
@@ -2,6 +2,34 @@ package oop;
 
 public class SodaCan {
 
+    // Instance variables (or fields) are normally private
+    private int height;
+    private int radius;
+    private double surfaceArea;
+    private double volume;
+
+    // Constructors - are called to initialize fields during object creation
+    public SodaCan(int height, int radius) {
+        this.height = height;
+        this.radius = radius;
+    }
+
+    // The "no-argument" (default) constructor
+    public SodaCan()
+    {
+        height = 0;
+        radius = 0;
+    }
+
+    public double getSurfaceArea(int height, int radius){
+        surfaceArea = (2 * 3.14 * (this.radius * this.height)) + (2 * 3.14 * (this.radius * this.radius));
+        return surfaceArea;
+    }
+
+    public double getVolume(){
+        volume = this.height * (3.14 * (this.radius*this.radius) );
+        return volume;
+    }
     //TODO
     // see P8.5 for class requirements
 }
